89c3f6323b11f4899ad01cb68cab237b62249831
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-09-12 16:22:56 -0500

    add more args

--- decode_WEBNLG.sh
+++ decode_WEBNLG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -lt 4 ]; then
-  echo "./decode_WEBNLG.sh <model> <checkpoint> <gpu_id> <data_part>"
+if [ "$#" -lt 6 ]; then
+  echo "./decode_WEBNLG.sh <model> <checkpoint> <gpu_id> <dataset> <experiment name> <data_part>"
   exit 2
 fi
 
@@ -9,7 +9,7 @@ if [[ ${1} == *"bart"* ]]; then
   bash webnlg/test_graph2text_bart.sh ${1} ${2} ${3}
 fi
 if [[ ${1} == *"t5"* ]]; then
-  bash webnlg/test_graph2text.sh ${1} ${2} ${3} ${4}
+  bash webnlg/test_graph2text.sh ${1} ${2} ${3} ${4} ${5} ${6}
 fi
 
 
--- webnlg/test_graph2text.sh
+++ webnlg/test_graph2text.sh
@@ -5,8 +5,10 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 GPUID=$3
 CHECK_POINT=$2
 MODEL=$1
-DATA_PART=$4
-FOLDER=outputs/test_model/${DATA_PART}
+DATASET=$4
+EXPT_NAME=$5
+DATA_PART=$6
+FOLDER=outputs/test_model/${DATASET}/${EXPT_NAME}/${DATA_PART}
 
 export CURRENT_DIR=${ROOT_DIR}
 export OUTPUT_DIR=${CURRENT_DIR}/${FOLDER}
@@ -20,7 +22,7 @@ export OMP_NUM_THREADS=10
 export CUDA_VISIBLE_DEVICES=${GPUID}
 #--n_test 20 \
 python ${ROOT_DIR}/finetune.py \
---data_dir=${ROOT_DIR}/data/webnlg/${DATA_PART} \
+--data_dir=${ROOT_DIR}/data/webnlg/${DATASET}/${EXPT_NAME}/${DATA_PART} \
 --task graph2text \
 --model_name_or_path=${MODEL} \
 --eval_batch_size=8 \

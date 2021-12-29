9c9f91cfefa1d2d7ae1193fbae2e99fab25edd00
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-12-28 18:58:54 -0600

    update entity tags

--- webnlg/data/generate_template.py
+++ webnlg/data/generate_template.py
@@ -10,10 +10,7 @@ import os
 from fuzzywuzzy import fuzz 
 import spacy
 from tqdm import tqdm
-
-folder = sys.argv[1]
-
-datasets = ['train', 'dev', 'test_both', 'test_seen', 'test_unseen']
+from template_data import TemplateTag 
 
 def camel_case_split(identifier):
     matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
@@ -94,13 +91,16 @@ def process_triples(mtriples):
         edges = ' '.join(edge_split)
         
         sub = {}
-        sub['tokens'] = nodes1.split() 
+        sub['tokens'] = nodes1.split()
+        sub['role'] = 'sub' 
 
         rel = {}
         rel['tokens'] = edges.split()
+        rel['role'] = 'rel'
 
         obj = {}
         obj['tokens'] = nodes2.split()
+        obj['role'] = 'obj'
 
         triple_info = {
             'sub': sub,
@@ -112,44 +112,50 @@ def process_triples(mtriples):
     nodes, ent_info_dict = triple2node(rel_triple_lst)
     return nodes, ent_info_dict
 
+
 def triple2node(rel_triple_lst):
     nodes = []
     ent_idx = 1
     ent_info_dict = {}
+    nodes.append(TemplateTag.Para_Start)
     for triple in rel_triple_lst:
-        nodes.append('[T]')
+        nodes.append(TemplateTag.Triple_Start)
         for key in ['sub', 'obj']:
             ent_src_text = ' '.join(triple[key]['tokens'])
             ent_text = ent_src_text.lower()
             if ent_text not in ent_info_dict:
-                ent_info_dict[ent_text] = {'text':ent_src_text, 'ent_idx':ent_idx }
+                ent_info_dict[ent_text] = {'text':ent_src_text, 'entity_id':ent_idx, 'role':triple[key]['role'] }
                 ent_idx += 1
-            triple[key]['idx'] = ent_info_dict[ent_text]['ent_idx']
-            tag_idx = triple[key]['idx']
-            triple[key]['start_tag'] = '[E%d]' % tag_idx
-            triple[key]['end_tag'] = '[/E%d]' % tag_idx
+            #triple[key]['idx'] = ent_info_dict[ent_text]['ent_idx']
+            #tag_idx = triple[key]['idx']
+            #triple[key]['start_tag'] = '[E%d]' % tag_idx
+            #triple[key]['end_tag'] = '[/E%d]' % tag_idx
         
         sub = triple['sub']
         sub_toks = []
-        sub_toks.append(sub['start_tag'])
+        sub_toks.append(TemplateTag.Subject_Start)
         sub_toks.extend(sub['tokens'])
-        sub_toks.append(sub['end_tag'])
+        sub_toks.append(TemplateTag.Subject_End)
 
         rel = triple['rel']
         rel_toks = []
-        rel_toks.append('[R]')
+        rel_toks.append(TemplateTag.Rel_Start)
         rel_toks.extend(rel['tokens'])
+        rel_toks.append(TemplateTag.Rel_End)
         
         obj = triple['obj']
         obj_toks = []
-        obj_toks.append(obj['start_tag'])
+        obj_toks.append(TemplateTag.Object_Start)
         obj_toks.extend(obj['tokens'])
-        obj_toks.append(obj['end_tag'])
+        obj_toks.append(TemplateTag.Object_End)
        
         nodes.extend(sub_toks)
         nodes.extend(rel_toks)
         nodes.extend(obj_toks)
+
+        nodes.append(TemplateTag.Triple_End)
         
+    nodes.append(TemplateTag.Para_End)
     return nodes, ent_info_dict 
 
 Ent_Not_found = 0
@@ -275,17 +281,25 @@ def fuzzy_match_entity(ent_text, target_text):
          
     return idx, span_len
 
+def get_entity_tags(role):
+    if role == 'sub':
+        return (TemplateTag.Subject_Start, TemplateTag.Subject_End)
+    elif role == 'obj'
+        return (TemplateTag.Object_Start, TemplateTag.Object_End)
+    else:
+        raise ValueError('No supported role [%s]' % role)
+
 def get_template_output(target_text, ent_info_dict):
     out_info_dict = {}
     out_text = target_text
 
     ent_text_lst =[(ent, len(ent)) for ent in ent_info_dict]
     ent_text_lst = sorted(ent_text_lst, key=lambda x: x[1], reverse=True)
-    for ent_text, _ in ent_text_lst:
+    for ent_text, _ in ent_text_lst: # for each entity in a triple, find the match in target text 
         out_text_lower = out_text.lower()
-        ent_idx = ent_info_dict[ent_text]['ent_idx']
+        role = ent_info_dict[ent_text]['role']
+        ent_idx = ent_info_dict[ent_text]['entity_id'] 
         idx = out_text_lower.find(ent_text)
-        
         span_len = len(ent_text) 
         if idx == -1:
             idx, span_len = fuzzy_match_entity(ent_text, out_text_lower)
@@ -295,126 +309,133 @@ def get_template_output(target_text, ent_info_dict):
         out_ent = out_text[idx:(idx+span_len)]
         ent_to_replace = out_ent
          
-        template_text = '[E%d][/E%d]' % (ent_idx, ent_idx)
-        out_info_dict[template_text] = '[E%d] %s [/E%d]' % (ent_idx, ent_info_dict[ent_text]['text'], ent_idx)
+        template_text = '<e_n_t_%d></e_n_t_%d>' % (ent_idx, ent_idx)
+        start_tag, end_tag = get_entity_tags(role)
+        out_info_dict[template_text] = '%s %s %s' % (start_tag, ent_info_dict[ent_text]['text'], end_tag)
         to_replaced = re.compile(re.escape(ent_to_replace), re.IGNORECASE)
+        #replace the entity in target (out) text with template text 
         out_text = to_replaced.sub(template_text, out_text)
     
     for template_text in out_info_dict:
         out_text = out_text.replace(template_text, out_info_dict[template_text])
     return out_text
 
-global nlp
+def main():
+    folder = sys.argv[1]
+    datasets = ['train', 'dev', 'test_both', 'test_seen', 'test_unseen']
+    
+    global nlp
+    nlp = spacy.load("en_core_web_sm")
 
-nlp = spacy.load("en_core_web_sm")
+    train_cat = set()
+    dataset_points = []
+    for d in tqdm(datasets):
+        cont_all = 0
+        datapoints = []
+        all_cats = set()
+        if 'unseen' in d:
+            d_set = 'test'
+            files = [folder + '/' + d_set + '/testdata_unseen_with_lex.xml']
+        elif 'test' in d:
+            d_set = 'test'
+            files = [folder + '/' + d_set + '/testdata_with_lex.xml']
+        else:
+            files = Path(folder + '/' + d).rglob('*.xml')
 
-train_cat = set()
-dataset_points = []
-for d in tqdm(datasets):
-    cont_all = 0
-    datapoints = []
-    all_cats = set()
-    if 'unseen' in d:
-        d_set = 'test'
-        files = [folder + '/' + d_set + '/testdata_unseen_with_lex.xml']
-    elif 'test' in d:
-        d_set = 'test'
-        files = [folder + '/' + d_set + '/testdata_with_lex.xml']
-    else:
-        files = Path(folder + '/' + d).rglob('*.xml')
+        files = sorted(list(files))
 
-    files = sorted(list(files))
+        for idx, filename in tqdm(enumerate(files), total=len(files)):
+            #print(filename)
+            filename = str(filename)
 
-    for idx, filename in tqdm(enumerate(files), total=len(files)):
-        #print(filename)
-        filename = str(filename)
-
+            if d == 'train':
+                datapoint, cats, cont = get_data(filename)
+            else:
+                datapoint, cats, cont = get_data_dev_test(filename, train_cat, d)
+            cont_all += cont
+            all_cats.update(cats)
+            datapoints.extend(datapoint)
         if d == 'train':
-            datapoint, cats, cont = get_data(filename)
-        else:
-            datapoint, cats, cont = get_data_dev_test(filename, train_cat, d)
-        cont_all += cont
-        all_cats.update(cats)
-        datapoints.extend(datapoint)
-    if d == 'train':
-        train_cat = all_cats
-    print(d, len(datapoints))
-    print('cont', cont_all)
-    print('len cat', len(all_cats))
-    print('cat', all_cats)
-    dataset_points.append(datapoints)
+            train_cat = all_cats
+        print(d, len(datapoints))
+        print('cont', cont_all)
+        print('len cat', len(all_cats))
+        print('cat', all_cats)
+        dataset_points.append(datapoints)
 
 
-path = os.path.dirname(os.path.realpath(__file__)) + '/webnlg/'
-if not os.path.exists(path):
-    os.makedirs(path)
+    path = os.path.dirname(os.path.realpath(__file__)) + '/webnlg/'
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-os.system("rm " + path + '/*')
+    os.system("rm " + path + '/*')
 
-for idx, datapoints in enumerate(dataset_points):
+    for idx, datapoints in enumerate(dataset_points):
 
-    part = datasets[idx]
+        part = datasets[idx]
 
-    if part == 'dev':
-        part = 'val'
+        if part == 'dev':
+            part = 'val'
 
-    nodes = []
-    surfaces = []
-    surfaces_2 = []
-    surfaces_3 = []
+        nodes = []
+        surfaces = []
+        surfaces_2 = []
+        surfaces_3 = []
 
-    surfaces_eval = []
-    surfaces_2_eval = []
-    surfaces_3_eval = []
-    for datapoint in datapoints:
-        node = datapoint[0]
-        sur = datapoint[1]
-        nodes.append(' '.join(node))
+        surfaces_eval = []
+        surfaces_2_eval = []
+        surfaces_3_eval = []
+        for datapoint in datapoints:
+            node = datapoint[0]
+            sur = datapoint[1]
+            nodes.append(' '.join(node))
+            if part != 'train':
+                surfaces.append(sur[0][0])
+                surfaces_eval.append(sur[0][1])
+                if len(sur) > 1:
+                    surfaces_2.append(sur[1][0])
+                    surfaces_2_eval.append(sur[1][1])
+                else:
+                    surfaces_2.append('')
+                    surfaces_2_eval.append('')
+                if len(sur) > 2:
+                    surfaces_3.append(sur[2][0])
+                    surfaces_3_eval.append(sur[2][1])
+                else:
+                    surfaces_3.append('')
+                    surfaces_3_eval.append('')
+            else:
+                surfaces.append(sur[0])
+                surfaces_eval.append(sur[1])
+
+        with open(path + '/' + part + '.source', 'w', encoding='utf8') as f:
+            f.write('\n'.join(nodes))
+            f.write('\n')
+        with open(path + '/' + part + '.target', 'w', encoding='utf8') as f:
+            f.write('\n'.join(surfaces))
+            f.write('\n')
         if part != 'train':
-            surfaces.append(sur[0][0])
-            surfaces_eval.append(sur[0][1])
-            if len(sur) > 1:
-                surfaces_2.append(sur[1][0])
-                surfaces_2_eval.append(sur[1][1])
-            else:
-                surfaces_2.append('')
-                surfaces_2_eval.append('')
-            if len(sur) > 2:
-                surfaces_3.append(sur[2][0])
-                surfaces_3_eval.append(sur[2][1])
-            else:
-                surfaces_3.append('')
-                surfaces_3_eval.append('')
-        else:
-            surfaces.append(sur[0])
-            surfaces_eval.append(sur[1])
+            with open(path + '/' + part + '.target2', 'w', encoding='utf8') as f:
+                f.write('\n'.join(surfaces_2))
+                f.write('\n')
+            with open(path + '/' + part + '.target3', 'w', encoding='utf8') as f:
+                f.write('\n'.join(surfaces_3))
+                f.write('\n')
 
-    with open(path + '/' + part + '.source', 'w', encoding='utf8') as f:
-        f.write('\n'.join(nodes))
-        f.write('\n')
-    with open(path + '/' + part + '.target', 'w', encoding='utf8') as f:
-        f.write('\n'.join(surfaces))
-        f.write('\n')
-    if part != 'train':
-        with open(path + '/' + part + '.target2', 'w', encoding='utf8') as f:
-            f.write('\n'.join(surfaces_2))
+        with open(path + '/' + part + '.target_eval', 'w', encoding='utf8') as f:
+            f.write('\n'.join(surfaces_eval))
             f.write('\n')
-        with open(path + '/' + part + '.target3', 'w', encoding='utf8') as f:
-            f.write('\n'.join(surfaces_3))
-            f.write('\n')
+        if part != 'train':
+            with open(path + '/' + part + '.target2_eval', 'w', encoding='utf8') as f:
+                f.write('\n'.join(surfaces_2_eval))
+                f.write('\n')
+            with open(path + '/' + part + '.target3_eval', 'w', encoding='utf8') as f:
+                f.write('\n'.join(surfaces_3_eval))
+                f.write('\n')
 
-    with open(path + '/' + part + '.target_eval', 'w', encoding='utf8') as f:
-        f.write('\n'.join(surfaces_eval))
-        f.write('\n')
-    if part != 'train':
-        with open(path + '/' + part + '.target2_eval', 'w', encoding='utf8') as f:
-            f.write('\n'.join(surfaces_2_eval))
-            f.write('\n')
-        with open(path + '/' + part + '.target3_eval', 'w', encoding='utf8') as f:
-            f.write('\n'.join(surfaces_3_eval))
-            f.write('\n')
-
-print('Ent_Not_found %d, Ent_Found %d' % (Ent_Not_found, Ent_Found))
+    print('Ent_Not_found %d, Ent_Found %d' % (Ent_Not_found, Ent_Found))
 
 
+if __name__ == '__main__':
+    main()
 
